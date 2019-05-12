cefb72c008194ae27390ddedef4984b14ce0bac5
Loke <loke202@gmail.com>
2019-05-12 12:35:15 -0600

    Changes to be committed: 	modified:   app.js 	modified:   package-lock.json 	new file:   public/exsys.css 	modified:   public/index.html

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const express = require('express'); // Express web server framework
+const cors = require('cors');
 const supersaas = require('supersaas-api-client');
 const Client = supersaas.Client;
+const schedule = 'stadsarkiv'
+const scheduleId = 419334
 
 const app = express();
 
 app.use(express.static(`${__dirname}/public`))
 
+app.use(express.json());
+
+app.use(cors());
+
 Client.configure({
   accountName: 'Loke',
   api_key: 'pRs3yLRqbkYwVx_7Gd5oTg',
-  host: 'http://test',
-  dryRun: true,
-  verbose: true
+  dryRun: false,
+  verbose: false
 })
+
+app.get('/free', (req, res) => {
+  const resource = req.resource;
+
+  Client.Instance.appointments.available(scheduleId, '2019-5-11 00:00:00', resource, function(err, data) { 
+    console.log(data); //=> ["Appointment", ...]
+  });
+});
+
+app.post('/tos', (req, res) => {
+  console.log(req.body);
+  res.send('got it');
+});
+
+app.get('/sass', (req, res) => {
+  console.log('hi');
+  $.get('https://www.supersaas.dk/schedule/Loke/stadsarkiv', function(data) {
+    console.log(data);
+  });
+});
 
 app.listen(80);
